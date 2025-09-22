c38d80f73c1dcbb5b8c033e8981403f552fcf2c6
Bérénice Bettinger <berenice.bettinger@edu.esiee.fr>
2025-09-22 15:27:32 +0000

    si p est associé à 1 ou 0 alors ce n'est pas un nombre premier, je prends un diviseur qui va de 2 au nombre indiquée. Et si le reste de la division est égale à 0 alors les nombre n'est pas un nombre premier sinon on retourne true en disant donc que c'est un nombre premier

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from math import sqrt
 
-#### Fonction secondaire
+"""
+Module pour tester et afficher les nombres premiers jusqu'à 100.
+"""
 
 
 def isprime(p):
+    """Retourne True si p est un nombre premier, False sinon."""
+    if p in (0, 1):
+        return False
+    for d in range(2, p):
+        if p % d == 0:
+            return False
+    return True
+print (isprime(20))
 
-    # votre code ici
-
-    pass
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Affiche les nombres premiers de 0 à 99."""
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
 
 
